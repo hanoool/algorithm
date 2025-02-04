c1382aad1e443845c21b2c348703d29482e7e333
hanwool <hanwe92@gmail.com>
2025-02-04 17:50:24 +0900

    떡볶이 떡 만들기 dp 정답 풀이

--- src/this_is_coding_test/binary_search/떡볶이 떡 만들기.py
+++ src/this_is_coding_test/binary_search/떡볶이 떡 만들기.py
@@ -1,20 +1,20 @@
 def binary_search(arr, target, start, end):
+    result = 0
     while start <= end:
         mid = (start + end) // 2
-        arr_sum = sum([(x-mid) if x-mid > 0 else 0 for x in arr])
-        if arr_sum == target:
-            print(mid)
-            break
-        elif arr_sum > target:
-            start = mid + 1
-        else:
+        arr_sum = sum([max(x-mid, 0)for x in arr])
+        if arr_sum < target:
             end = mid - 1
+        else:
+            result = mid
+            start = mid + 1
+    return result
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
     arr = list(map(int, input().split()))
     
     # 적어도 m 만큼의 떡을 얻기 위해 설정할 수 있는 절단기 높이의 최대값
-    binary_search(arr, m, 0, max(arr))
+    print(binary_search(arr, m, 0, max(arr)))
     
     
