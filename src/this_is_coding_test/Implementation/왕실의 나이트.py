bc591527802a79cd0458bcaec2ef3923142d984c
hanwool <hanwe92@gmail.com>
2025-02-10 16:20:26 +0900

    왕실의 나이트: 해답 풀이

--- src/this_is_coding_test/Implementation/왕실의 나이트.py
+++ src/this_is_coding_test/Implementation/왕실의 나이트.py
@@ -1,26 +1,15 @@
 if __name__ == "__main__":
     input_data = input()
-    w = input_data[0]
-    h = int(input_data[1])
-    change = {'a': 1,'b': 2,'c': 3,'d': 4,'e': 5,'f': 6,'g': 7,'h': 8}
+    column = int(ord(input_data[0]) - ord('a')) + 1
+    row = int(input_data[1])
+    steps = [(2,-1),(2,1),(-2,-1),(-2,1),(1,2),(1,-2),(-1,2),(-1,-2)]
     count = 0
     
-    if change[w]+2 <= 8 and h + 1 <= 8:
-        count += 1
-    if change[w]+2 <= 8 and h - 1 >= 1:
-        count += 1
-    if change[w]-2 >= 1 and h + 1 <= 8:
-        count += 1
-    if change[w]-2 >= 1 and h - 1 <= 1:
-        count += 1
-    if change[w]+1 <= 8 and h + 2 <= 8:
-        count += 1
-    if change[w]+1 <= 8 and h - 2 >= 1:
-        count += 1
-    if change[w]-1 >= 1 and h + 2 <= 8:
-        count += 1
-    if change[w]-1 >= 1 and h - 2 <= 1:
-        count += 1
+    for step in steps:
+        next_column = column + step[0]
+        next_row = column + step[1]
+        if next_column <= 8 and next_row >= 1 and next_row <= 8 and next_column >= 1:
+            count += 1
     
     print(count)
     
