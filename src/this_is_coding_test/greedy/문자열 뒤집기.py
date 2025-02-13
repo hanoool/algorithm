73317eda6a7a309c991f417068dc96af327954aa
hanwool <hanwe92@gmail.com>
2025-02-13 14:05:15 +0900

    문자열 뒤집기: 해답

--- src/this_is_coding_test/greedy/문자열 뒤집기.py
+++ src/this_is_coding_test/greedy/문자열 뒤집기.py
@@ -2,18 +2,17 @@ if __name__ == "__main__":
     s = input()
     group = 0
     count = [0,0]
+
+    if s[0] == '0':
+        count[0] += 1
+    else:
+        count[1] += 1    
     
     for i in range(len(s) - 1):
-       if s[i] == s[i + 1]:
-           if i + 1 == len(s):
-            if s[i] == '0':
-               count[0] += 1
-            else:
-               count[1] += 1     
-           else:
+        if s[i] == s[i + 1]:
             continue
-       else:
-           if s[i] == '0':
+        else:
+           if s[i + 1] == '0':
                count[0] += 1
            else:
                count[1] += 1
