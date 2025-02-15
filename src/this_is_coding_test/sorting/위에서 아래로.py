4b27745aafdf26aded1274789e5a282bb69bb498
hanwool <hanwe92@gmail.com>
2025-02-15 10:38:42 +0900

    정렬: 위에서 아래로

---
+++ src/this_is_coding_test/sorting/위에서 아래로.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    n = int(input())
+    arr = []
+    for i in range(n):
+        arr.append(int(input()))
+    arr.sort(reverse=True)
+    
+    for i in arr:
+        print(i, end=" ")
