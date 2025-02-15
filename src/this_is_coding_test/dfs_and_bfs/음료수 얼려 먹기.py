4caf9268f2dc4054e65437390c1d8f3de98d924d
hanwool <hanwe92@gmail.com>
2025-02-15 20:21:01 +0900

    음료수 얼려 먹기: dfs 해답

--- src/this_is_coding_test/dfs_and_bfs/음료수 얼려 먹기.py
+++ src/this_is_coding_test/dfs_and_bfs/음료수 얼려 먹기.py
@@ -1,16 +1,15 @@
 if __name__ == "__main__":
     def dfs(i, j):
-        global count
-        if i < 0 or j < 0:
-            return None
-        if i >= n or j >= m:
-            return None
-        if visited[i][j] != 1 and map_info[i][j] != 1:
-            visited[i][j] = 1
+        if i < 0 or j < 0 or i >= n or j >= m:
+            return False
+        if map_info[i][j] != 1:
+            map_info[i][j] = 1
             dfs(i-1, j)
             dfs(i+1, j)
             dfs(i, j-1)
             dfs(i, j+1)
+            return True
+        return False
     
     n,m = map(int, input().split())
     map_info = []
@@ -18,15 +17,11 @@ if __name__ == "__main__":
     for i in range(n):
         map_info.append(list(map(int, input())))
         
-    print(map_info)
-    visited = [[0 for _ in range(m)] for _ in range(n)]
     count = 0
-    print(f"visited: {visited}")
     
     for i in range(len(map_info)):
         for j in map_info[i]:
-            if visited[i][j] == 0 and map_info[i][j] == 0:
-                dfs(i, j)
+            if dfs(i, j) == True:
                 count += 1
     print(count)
             
